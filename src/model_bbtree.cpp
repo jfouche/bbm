jfouche/bbm

commit a5edc9b25a7bf485d6cd758bf8f7ef4cd83a1376

fixed removing BB

src/model_bbtree.cpp
@@ -8,6 +8,7 @@ enum Columns {
     COL_COUNT
 };
 
+// ===========================================================================
 
 TreeItem::TreeItem(DataModel* model, BuildingBlockTreeModel* treeModel)
     : m_parent(nullptr)
@@ -25,8 +26,11 @@ TreeItem::TreeItem(BuildingBlock* bb, TreeItem* parent, BuildingBlockTreeModel* 
     , m_treeModel(treeModel)
     , m_bb(bb)
 {
-    connect(bb, SIGNAL(childAdded(BuildingBlock*)), this, SLOT(add(BuildingBlock*)));
-    connect(bb, SIGNAL(childRemoved(BuildingBlock*)), this, SLOT(remove(BuildingBlock*)));
+    connect(bb, SIGNAL(childAdded(BuildingBlock*, BuildingBlock*)), this, SLOT(add(BuildingBlock*, BuildingBlock*)));
+    connect(bb, SIGNAL(childRemoved(BuildingBlock*, BuildingBlock*)), this, SLOT(remove(BuildingBlock*, BuildingBlock*)));
+    for (auto bb : m_bb->children()) {
+        appendChild(new TreeItem(bb, this, m_treeModel));
+    }
 }
 
 TreeItem::~TreeItem()
@@ -34,23 +38,36 @@ TreeItem::~TreeItem()
     qDeleteAll(m_children);
 }
 
-void TreeItem::add(BuildingBlock* bb)
+void TreeItem::add(BuildingBlock* bb, BuildingBlock* parent)
 {
-    m_treeModel->beginResetModel();
+    Q_ASSERT(data() == parent);
+
+    const auto parentIdx = m_treeModel->createIndex(row(), 0, this);
+    const int first = m_children.size();
+    m_treeModel->beginInsertRows(parentIdx, first, first);
     appendChild(new TreeItem(bb, this, m_treeModel));
-    m_treeModel->endResetModel();
+    m_treeModel->endInsertRows();
 }
 
-void TreeItem::remove(BuildingBlock* bb)
+void TreeItem::remove(BuildingBlock* bb, BuildingBlock* parent)
 {
-    m_treeModel->beginResetModel();
-    for (auto it = m_children.begin(); it != m_children.end(); ++it) {
-        TreeItem* item = *it;
+    Q_ASSERT(data() == parent);
+
+    int pos = 0;
+    for (auto it = m_children.begin(); it != m_children.end(); /* no it++ */) {
+        const TreeItem* item = *it;
         if (item->m_bb == bb) {
+            const auto parentIdx = m_treeModel->createIndex(row(), 0, this);
+            m_treeModel->beginRemoveRows(parentIdx, pos, pos);
             it = m_children.erase(it);
+            delete item;
+            m_treeModel->endRemoveRows();
         }
+        else {
+            ++it;
+        }
+        pos++;
     }
-    m_treeModel->endResetModel();
 }
 
 void TreeItem::appendChild(TreeItem *child)
@@ -98,6 +115,7 @@ TreeItem* TreeItem::parentItem()
 }
 
 // ===========================================================================
+
 BuildingBlockTreeModel::BuildingBlockTreeModel(DataModel* model, QObject *parent)
     : QAbstractItemModel(parent)
     , m_model(model)

src/datamodel.cpp
@@ -42,7 +42,7 @@ void BuildingBlock::add(BuildingBlock *bb)
     int index = m_children.indexOf(bb);
     if (index == -1) {
         m_children.append(bb);
-        emit childAdded(bb);
+        emit childAdded(bb, this);
     }
 }
 
@@ -51,7 +51,7 @@ void BuildingBlock::remove(BuildingBlock* bb)
     int index = m_children.indexOf(bb);
     if (index != -1) {
         m_children.removeAt(index);
-        emit childRemoved(bb);
+        emit childRemoved(bb, this);
     }
 }
 

src/datamodel.h
@@ -51,8 +51,8 @@ public:
 
 signals:
     void changed();
-    void childAdded(BuildingBlock* child);
-    void childRemoved(BuildingBlock* child);
+    void childAdded(BuildingBlock* child, BuildingBlock* parent);
+    void childRemoved(BuildingBlock* child, BuildingBlock* parent);
 
 private:
     QString m_name;

src/model_bbtree.h
@@ -8,6 +8,9 @@ class BuildingBlock;
 class TreeItem;
 class BuildingBlockTreeModel;
 
+/**
+ * @brief The TreeItem class
+ */
 class TreeItem : public QObject
 {
     Q_OBJECT
@@ -25,8 +28,8 @@ public:
     TreeItem *parentItem();
 
 private slots:
-    void add(BuildingBlock* bb);
-    void remove(BuildingBlock* bb);
+    void add(BuildingBlock* bb, BuildingBlock* parent);
+    void remove(BuildingBlock* bb, BuildingBlock* parent);
 
 private:
     QList<TreeItem*> m_children;
@@ -35,6 +38,9 @@ private:
     BuildingBlock* m_bb;
 };
 
+/**
+ * @brief The BuildingBlockTreeModel class
+ */
 class BuildingBlockTreeModel : public QAbstractItemModel
 {
     Q_OBJECT

commit 659ce93af43a58ebcc8d602231e9c45cc89fcef4

fixed regression introduced in a5edc9b, that prevented to add a BB to the DataModel

src/model_bbtree.cpp
@@ -45,18 +45,20 @@ TreeItem::~TreeItem()
 
 void TreeItem::add(BuildingBlock* bb)
 {
-    add(bb, nullptr);
+    QModelIndex parentIdx = QModelIndex();
+    if (m_bb) {
+        parentIdx = m_treeModel->createIndex(row(), 0, this);
+    }
+    const int first = m_children.size();
+    m_treeModel->beginInsertRows(parentIdx, first, first);
+    appendChild(new TreeItem(bb, this, m_treeModel));
+    m_treeModel->endInsertRows();
 }
 
 void TreeItem::add(BuildingBlock* bb, BuildingBlock* parent)
 {
     Q_ASSERT(data() == parent);
-
-    const auto parentIdx = m_treeModel->createIndex(row(), 0, this);
-    const int first = m_children.size();
-    m_treeModel->beginInsertRows(parentIdx, first, first);
-    appendChild(new TreeItem(bb, this, m_treeModel));
-    m_treeModel->endInsertRows();
+    add(bb);
 }
 
 void TreeItem::remove(BuildingBlock* bb, BuildingBlock* parent)